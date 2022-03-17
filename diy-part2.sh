3eb3f354a0dba04f05336dafab8f46b3a70500ec
WizardQAQ <lgl9693@gamil.com>
2022-03-17 16:54:57 +0800

    主题修改argonne

--- diy-part2.sh
+++ diy-part2.sh
@@ -13,6 +13,6 @@
 # Modify default IP
 sed -i 's/192.168.1.1/192.168.2.1/g' package/base-files/files/bin/config_generate
 # Modify default theme
-sed -i 's/luci-theme-bootstrap/luci-theme-argon/g' ./feeds/luci/collections/luci/Makefile
+sed -i 's/luci-theme-bootstrap/luci-theme-argonne/g' ./feeds/luci/collections/luci/Makefile
 # Enable ccache accelerate
 echo "CONFIG_CCACHE=y" >> ./.config
