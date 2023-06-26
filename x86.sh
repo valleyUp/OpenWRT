d48204014718959f9bb4c0b2688cf421730a5e12
Glenn <512641455@qq.com>
2023-06-26 13:31:41 +0800

    feat(x86): add x86 device

---
+++ x86.sh
@@ -0,0 +1,6 @@
+# use dhcpcd
+
+# Modify default theme
+sed -i 's/luci-theme-bootstrap/luci-theme-argone/g' ./feeds/luci/collections/luci/Makefile
+# Enable ccache accelerate
+echo "CONFIG_CCACHE=y" >> ./.config
